23e6beec8b4254b183f86a9300d4edbb08d0735e
kiyo0919 <kakinoki0919@gmail.com>
2021-07-07 14:36:19 +0000

    [finish]chapter5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a5505f1bb6f02864f3020ab64f69aa103add2809d56b033cf6508c7e96fce318a03f7da729b8f44f28faca2c0a4bad27a7e8ea69ebdf77aeed27c240e56a8dbe'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+   get 'home/about' => 'homes#about', as: "about"
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
