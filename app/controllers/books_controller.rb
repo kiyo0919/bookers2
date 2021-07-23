968bbc5fba395395ca1f62857ba7e9719daed4d3
kiyo0919 <kakinoki0919@gmail.com>
2021-07-23 01:10:51 +0000

    fix books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,10 +16,9 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @user = current_user
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render :index
